77608961501c28d03930c20893037e3117af60ae
ranitsarker <rntprince@gmail.com>
2023-12-21 22:29:43 +0600

    added jwt in cookie when login and when logout token remove from cookie

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:5174',
+  
+  ], 
+  credentials: true
+}));
 app.use(express.json());
-
-
-
-
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wgcoqid.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,6 +34,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // auth related endpoint or api
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      console.log('user for token:', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      
+      .send({success: true});
+    })
+    // user logout:
+    app.post('/logout', async(req, res) =>{
+      const user = req.body;
+      console.log('logged out user:', user)
+      res.clearCookie('token', 
+      {maxAge: 0,
+        sameSite: "none",
+        secure: true
+      }).send({success: true})
+    })
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -37,14 +71,9 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
-
 app.get('/', (req, res) => {
     res.send('task management server is running.')
 })
-
 app.listen(port, () => {
     console.log(`task management server is running on port ${port}`)
 })
